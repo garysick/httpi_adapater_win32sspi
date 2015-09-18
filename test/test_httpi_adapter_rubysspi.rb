1d002b947b22dd97f37c114b204b8466a12d32ad
Gary Sick <garys@cascadianmoonlight.com>
2015-09-18 12:27:24 -0700

    some refactoring to remove some duplication

--- test/test_httpi_adapter_rubysspi.rb
+++ test/test_httpi_adapter_rubysspi.rb
@@ -22,103 +22,102 @@ class TC_HttpiAdapterRubySSPI < MiniTest::Test
     assert_equal "Win32::SSPI::HttpClient", adapter.client.class.name
     assert_equal URI.parse(RequestURI), request.url
   end
-
-  def test_get
+  
+  def with_mock_adapter
     adapter_klass = create_mock_adapter
     HTTPI::Adapter.register(:ruby_sspi, adapter_klass, {})
 
-    request = HTTPI::Request.new(RequestURI)
-    request.headers[TestHeaderName] = TestHeader
-    request.headers[RemoteUserHdrName] = RemoteUserHdr
+    yield adapter_klass if block_given?
 
-    response = HTTPI::get(request, :ruby_sspi)
-
-    assert_equal 'HTTPI::Response', response.class.name
-    assert_equal 200, response.code
-    assert_equal 'hello test', response.body
-    assert_equal 1, response.headers.size
-    assert_equal AuthenticateHdr, response.headers[AuthenticateHdrName]
-
-    assert_equal request.url, adapter_klass.read_state(:uri)
-    assert_equal 'Win32::SSPI::HttpClient', adapter_klass.read_state(:adapter_client_klass)
-
-    http_req = adapter_klass.read_state(:http_request)
-    assert_equal 'Net::HTTP::Get', http_req.class.name
-    assert_equal "virtual-pc-serv.bpa.local", http_req.uri.host
-    assert_equal 3005, http_req.uri.port
-    assert_equal "/test", http_req.uri.path
-    
-    assert_equal TestHeader, http_req[TestHeaderName]
-    assert_equal RemoteUserHdr, http_req[RemoteUserHdrName]
-   ensure
     HTTPI::Adapter.register(:ruby_sspi,HTTPI::Adapter::RubySSPI,{})
+  end
+
+  def test_get
+    with_mock_adapter do |adapter_klass|
+      request = HTTPI::Request.new(RequestURI)
+      request.headers[TestHeaderName] = TestHeader
+      request.headers[RemoteUserHdrName] = RemoteUserHdr
+
+      response = HTTPI::get(request, :ruby_sspi)
+
+      assert_equal 'HTTPI::Response', response.class.name
+      assert_equal 200, response.code
+      assert_equal 'hello test', response.body
+      assert_equal 1, response.headers.size
+      assert_equal AuthenticateHdr, response.headers[AuthenticateHdrName]
+
+      assert_equal request.url, adapter_klass.read_state(:uri)
+      assert_equal 'Win32::SSPI::HttpClient', adapter_klass.read_state(:adapter_client_klass)
+
+      http_req = adapter_klass.read_state(:http_request)
+      assert_equal 'Net::HTTP::Get', http_req.class.name
+      assert_equal "virtual-pc-serv.bpa.local", http_req.uri.host
+      assert_equal 3005, http_req.uri.port
+      assert_equal "/test", http_req.uri.path
+      
+      assert_equal TestHeader, http_req[TestHeaderName]
+      assert_equal RemoteUserHdr, http_req[RemoteUserHdrName]
+    end
   end
 
   def test_get_with_query_parameter
-    adapter_klass = create_mock_adapter
-    HTTPI::Adapter.register(:ruby_sspi, adapter_klass, {})
+    with_mock_adapter do |adapter_klass|
+      request = HTTPI::Request.new(RequestURI)
+      request.query ="q=query"
+      request.headers[TestHeaderName] = TestHeader
+      request.headers[RemoteUserHdrName] = RemoteUserHdr
 
-    request = HTTPI::Request.new(RequestURI)
-    request.query ="q=query"
-    request.headers[TestHeaderName] = TestHeader
-    request.headers[RemoteUserHdrName] = RemoteUserHdr
+      response = HTTPI::get(request, :ruby_sspi)
 
-    response = HTTPI::get(request, :ruby_sspi)
+      assert_equal 'HTTPI::Response', response.class.name
+      assert_equal 200, response.code
+      assert_equal 'hello test', response.body
+      assert_equal 1, response.headers.size
+      assert_equal AuthenticateHdr, response.headers[AuthenticateHdrName]
 
-    assert_equal 'HTTPI::Response', response.class.name
-    assert_equal 200, response.code
-    assert_equal 'hello test', response.body
-    assert_equal 1, response.headers.size
-    assert_equal AuthenticateHdr, response.headers[AuthenticateHdrName]
+      assert_equal request.url, adapter_klass.read_state(:uri)
+      assert_equal 'Win32::SSPI::HttpClient', adapter_klass.read_state(:adapter_client_klass)
 
-    assert_equal request.url, adapter_klass.read_state(:uri)
-    assert_equal 'Win32::SSPI::HttpClient', adapter_klass.read_state(:adapter_client_klass)
-
-    http_req = adapter_klass.read_state(:http_request)
-    assert_equal 'Net::HTTP::Get', http_req.class.name
-    assert_equal "virtual-pc-serv.bpa.local", http_req.uri.host
-    assert_equal 3005, http_req.uri.port
-    assert_equal "/test", http_req.uri.path
-    assert_equal "q=query", http_req.uri.query
-    
-    assert_equal TestHeader, http_req[TestHeaderName]
-    assert_equal RemoteUserHdr, http_req[RemoteUserHdrName]
-   ensure
-    HTTPI::Adapter.register(:ruby_sspi,HTTPI::Adapter::RubySSPI,{})
+      http_req = adapter_klass.read_state(:http_request)
+      assert_equal 'Net::HTTP::Get', http_req.class.name
+      assert_equal "virtual-pc-serv.bpa.local", http_req.uri.host
+      assert_equal 3005, http_req.uri.port
+      assert_equal "/test", http_req.uri.path
+      assert_equal "q=query", http_req.uri.query
+      
+      assert_equal TestHeader, http_req[TestHeaderName]
+      assert_equal RemoteUserHdr, http_req[RemoteUserHdrName]
+    end
   end
 
   def test_post
-    adapter_klass = create_mock_adapter
-    HTTPI::Adapter.register(:ruby_sspi, adapter_klass, {})
+    with_mock_adapter do |adapter_klass|
+      request = HTTPI::Request.new(RequestURI)
+      request.body = "firstname=tom&lastname=johnson"
+      request.headers[TestHeaderName] = TestHeader
+      request.headers[RemoteUserHdrName] = RemoteUserHdr
 
-    request = HTTPI::Request.new(RequestURI)
-    request.body = "firstname=tom&lastname=johnson"
-    request.headers[TestHeaderName] = TestHeader
-    request.headers[RemoteUserHdrName] = RemoteUserHdr
+      response = HTTPI::post(request, :ruby_sspi)
 
-    response = HTTPI::post(request, :ruby_sspi)
+      assert_equal 'HTTPI::Response', response.class.name
+      assert_equal 200, response.code
+      assert_equal 'hello test', response.body
+      assert_equal 1, response.headers.size
+      assert_equal AuthenticateHdr, response.headers[AuthenticateHdrName]
 
-    assert_equal 'HTTPI::Response', response.class.name
-    assert_equal 200, response.code
-    assert_equal 'hello test', response.body
-    assert_equal 1, response.headers.size
-    assert_equal AuthenticateHdr, response.headers[AuthenticateHdrName]
+      assert_equal request.url, adapter_klass.read_state(:uri)
+      assert_equal 'Win32::SSPI::HttpClient', adapter_klass.read_state(:adapter_client_klass)
 
-    assert_equal request.url, adapter_klass.read_state(:uri)
-    assert_equal 'Win32::SSPI::HttpClient', adapter_klass.read_state(:adapter_client_klass)
-
-    http_req = adapter_klass.read_state(:http_request)
-    assert_equal 'Net::HTTP::Post', http_req.class.name
-    assert_equal "virtual-pc-serv.bpa.local", http_req.uri.host
-    assert_equal 3005, http_req.uri.port
-    assert_equal "/test", http_req.uri.path
-    assert_equal "firstname=tom&lastname=johnson", http_req.body
-    
-    assert_equal TestHeader, http_req[TestHeaderName]
-    assert_equal RemoteUserHdr, http_req[RemoteUserHdrName]
-    
-   ensure
-    HTTPI::Adapter.register(:ruby_sspi,HTTPI::Adapter::RubySSPI,{})
+      http_req = adapter_klass.read_state(:http_request)
+      assert_equal 'Net::HTTP::Post', http_req.class.name
+      assert_equal "virtual-pc-serv.bpa.local", http_req.uri.host
+      assert_equal 3005, http_req.uri.port
+      assert_equal "/test", http_req.uri.path
+      assert_equal "firstname=tom&lastname=johnson", http_req.body
+      
+      assert_equal TestHeader, http_req[TestHeaderName]
+      assert_equal RemoteUserHdr, http_req[RemoteUserHdrName]
+    end
   end
 
   def create_mock_adapter
