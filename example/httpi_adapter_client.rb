9eb24865b692c1a03d74430882288b12deddc2d1
Gary Sick <garys@cascadianmoonlight.com>
2015-09-30 07:12:32 -0700

    rename class; remove the -Ilib from the usage message

--- example/httpi_adapter_client.rb
+++ example/httpi_adapter_client.rb
@@ -2,7 +2,7 @@ require 'pp'
 require 'httpi'
 require 'httpi-adapter-win32sspi'
 
-class Win32SSPIClient
+class HttpiAdapterClient
   def self.run(url)
     uri = URI.parse(url)
     request = HTTPI::Request.new(url)
@@ -14,9 +14,9 @@ end
 
 if __FILE__ == $0
   if ARGV.length < 1
-    puts "usage: ruby -Ilib httpi_adapter_client url"
+    puts "usage: ruby httpi_adapter_client url"
     exit(0)
   end
 
-  Win32SSPIClient.run(ARGV[0])
+  HttpiAdapterClient.run(ARGV[0])
 end
